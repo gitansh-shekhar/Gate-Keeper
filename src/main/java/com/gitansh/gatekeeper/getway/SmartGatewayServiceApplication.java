1993324499cbe77b9ca1a0e28e8170cb219e8925
Gitansh Shekhar <gitanshshekhar2541@gmail.com>
2026-09-17 10:40:25 +0530

    Smart gateway service added

---
+++ src/main/java/com/gitansh/gatekeeper/getway/SmartGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gitansh.gatekeeper.getway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitansh/gatekeeper/getway/SmartGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitansh.gatekeeper.getway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
